6c9af569ac55bfa88ca73810507d04193edd587f
Simon Hamp <simon.hamp@me.com>
2024-10-29 20:07:07 +0000

    Move checkbox

--- resources/views/livewire/child-process.blade.php
+++ resources/views/livewire/child-process.blade.php
@@ -4,18 +4,18 @@
            class="transition duration-100 rounded-lg focus:border-transparent border border-gray-200 ring-offset-0 ring-gray-200 focus:ring-[#3A6CD9]/50 focus:ring-4"
            placeholder="Alias (leave empty for random)"/>
 
-    <button
-        wire:click="start"
-        class="bg-gradient-to-b from-[#4B91F7] to-[#367AF6] rounded-lg text-white py-1 px-2 shadow">
-        Start Process
-    </button>
-
     <label class="flex items-center space-x-2">
         <input type="checkbox"
                wire:model="persistent"
                class="transition duration-100 rounded border border-gray-800 focus:ring-0"/>
         <span class="ml-2 mt-0 text-gray-500 inline-block">Persistent</span>
     </label>
+
+    <button
+        wire:click="start"
+        class="bg-gradient-to-b from-[#4B91F7] to-[#367AF6] rounded-lg text-white py-1 px-2 shadow">
+        Start Process
+    </button>
 
     <button
         wire:click="stop"
