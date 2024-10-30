d397c703f0358fa8c86f8dd74ee8bde65eb169da
Simon Hamp <simon.hamp@me.com>
2024-10-30 01:32:43 +0000

    More window demos

--- app/Livewire/Window.php
+++ app/Livewire/Window.php
@@ -11,13 +11,42 @@ use Native\Laravel\Facades\Window as NativeWindow;
 class Window extends Component
 {
     public $title = 'NativePHP';
+    public $showDevTools = false;
     public $windowId = null;
+    public $closable = true;
+    public $alwaysOnTop = false;
 
     public function open()
     {
-        $window = NativeWindow::open($this->windowId ?? uniqid())
+        NativeWindow::open($this->windowId ?? uniqid())
             ->title($this->title)
-            ->url(url('/new-window'));
+            ->alwaysOnTop($this->alwaysOnTop)
+            ->url(url('/new-window'))
+            ->webPreferences([
+                'scrollBounce' => true,
+            ])
+            ->showDevTools($this->showDevTools);
+    }
+
+    public function setTitle()
+    {
+        NativeWindow::get($this->windowId)->title($this->title);
+    }
+
+    public function toggleClosable()
+    {
+        NativeWindow::get($this->windowId)->closable($this->closable = !$this->closable);
+    }
+
+    public function toggleDevTools()
+    {
+        $window = NativeWindow::get($this->windowId);
+
+        if ($window->devToolsOpen()) {
+            $window->hideDevTools();
+        } else {
+            $window->showDevTools();
+        }
     }
 
     public function render()
--- resources/views/livewire/window.blade.php
+++ resources/views/livewire/window.blade.php
@@ -5,15 +5,49 @@
                class="transition duration-100 rounded-lg focus:border-transparent border border-gray-200 ring-offset-0 ring-gray-200 focus:ring-[#3A6CD9]/50 focus:ring-4"
                placeholder="ID (leave empty for random)"/>
 
-        <input type="text"
-               wire:model="title"
-               class="transition duration-100 rounded-lg focus:border-transparent border border-gray-200 ring-offset-0 ring-gray-200 focus:ring-[#3A6CD9]/50 focus:ring-4"
-               placeholder="Title"/>
+        <div class="flex space-x-4 items-center">
+            <input type="text"
+                   wire:model="title"
+                   class="flex-1 transition duration-100 rounded-lg focus:border-transparent border border-gray-200 ring-offset-0 ring-gray-200 focus:ring-[#3A6CD9]/50 focus:ring-4"
+                   placeholder="Title"/>
+
+            <button
+                wire:click="setTitle"
+                class="bg-gradient-to-b from-[#4B91F7] to-[#367AF6] rounded-lg text-white py-1 px-2 shadow">
+                Update Title
+            </button>
+        </div>
+
+        <label class="flex items-center space-x-2">
+            <input type="checkbox"
+                   wire:model="showDevTools"
+                   class="transition duration-100 rounded border border-gray-800 focus:ring-0"/>
+            <span class="ml-2 mt-0 text-gray-500 inline-block">Show DevTools</span>
+        </label>
+
+        <label class="flex items-center space-x-2">
+            <input type="checkbox"
+                   wire:model="alwaysOnTop"
+                   class="transition duration-100 rounded border border-gray-800 focus:ring-0"/>
+            <span class="ml-2 mt-0 text-gray-500 inline-block">Always on Top</span>
+        </label>
 
         <button
             wire:click="open"
             class="bg-gradient-to-b from-[#4B91F7] to-[#367AF6] rounded-lg text-white py-1 px-2 shadow">
             Open Window
+        </button>
+
+        <button
+            wire:click="toggleDevTools"
+            class="bg-gradient-to-b from-[#4B91F7] to-[#367AF6] rounded-lg text-white py-1 px-2 shadow">
+            Toggle DevTools
+        </button>
+
+        <button
+            wire:click="toggleClosable"
+            class="bg-gradient-to-b from-[#4B91F7] to-[#367AF6] rounded-lg text-white py-1 px-2 shadow">
+            Toggle Closable
         </button>
     </div>
 </div>
