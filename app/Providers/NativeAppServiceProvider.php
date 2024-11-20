6ebfd8b9b9162a599cd2014e1d7ebe1f049ef9c3
Simon Hamp <simon.hamp@me.com>
2024-11-20 03:36:03 +0000

    Updated menu

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -2,25 +2,58 @@
 
 namespace App\Providers;
 
-use App\Events\MyCustomShortcutEvent;
-use App\Events\MenuBarClicked;
-use Native\Laravel\Facades\ContextMenu;
-use Native\Laravel\Facades\Dock;
+use Native\Laravel\Facades\Menu;
 use Native\Laravel\Facades\MenuBar;
 use Native\Laravel\Facades\Window;
-use Native\Laravel\GlobalShortcut;
-use Native\Laravel\Menu\Items\Checkbox;
-use Native\Laravel\Menu\Items\Radio;
-use Native\Laravel\Menu\Menu;
 use Native\Laravel\Window as AppWindow;
 
 class NativeAppServiceProvider
 {
     public function boot(): void
     {
+        // Menu::default();
+        Menu::create(
+            Menu::app(),
+            Menu::file(),
+            Menu::make(
+                Menu::goToRoute('home')
+                    ->label('Home')
+                    ->icon(public_path('medalTemplate.png')),
+                Menu::goToUrl(url('/window'))
+                    ->label('Windows'),
+                Menu::goToUrl('https://nativephp.com')
+                    ->label('External'),
+                Menu::label('Simon')
+                    ->id('simon')
+                    ->event(\Native\Laravel\Events\Menu\MenuItemClicked::class),
+            )->label('Navigation'),
+            Menu::make(
+                Menu::make(
+                    Menu::radio('Do you want this?'),
+                    Menu::radio('Or that?')
+                        ->tooltip('A little tip?'),
+                    Menu::separator(),
+                    Menu::radio('Yeh you do!')->id('yes'),
+                    Menu::radio('Nope')->disabled(),
+                )->label('Radios'),
+                Menu::make(
+                    Menu::checkbox('You want this?')
+                        ->checked(),
+                    Menu::checkbox('Yeh you do!')->id('yes'),
+                    Menu::checkbox('Nope')->disabled(),
+                )->label('Checkboxes'),
+                Menu::separator(),
+                Menu::fullscreen(),
+                Menu::separator(),
+                Menu::link('https://nativephp.com/', 'Docs'),
+                Menu::link('https://nativephp.com/docs/getting-started/sponsoring', 'Sponsor'),
+            )->label('Items'),
+        );
+
         Window::open()
             ->url(url('/'))
             ->titleBarHiddenInset()
+            ->fullscreenable(false)
             ->width(1024)
             ->height(768);
     }
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/app');
-});
+})->name('home');
 Route::get('/notifications', Notifications::class);
 Route::get('/clipboard', Clipboard::class);
 Route::get('/child-processes', ChildProcess::class);
