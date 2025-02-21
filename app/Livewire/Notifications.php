83fc300a1bc2d8b7f7e0538d9cd985cfcc475d67
Eser DENIZ <srwiez@gmail.com>
2025-02-21 13:03:10 +0100

    feat: added actions and reply to notifications

--- app/Livewire/Notifications.php
+++ app/Livewire/Notifications.php
@@ -4,7 +4,9 @@ namespace App\Livewire;
 
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Native\Laravel\Events\Notifications\NotificationActionClicked;
 use Native\Laravel\Events\Notifications\NotificationClicked;
+use Native\Laravel\Events\Notifications\NotificationReply;
 use Native\Laravel\Facades\Window;
 use Native\Laravel\Notification;
 
@@ -15,18 +17,44 @@ class Notifications extends Component
 
     public string $message = 'A notification from NativePHP';
 
+    public ?string $replyPlaceholder = null;
+
+    public bool $hasReply = false;
+
+    public array $actions = [];
+
     public bool $notificationClicked = false;
+
+    public ?string $notificationReplied = null;
+
+    public ?string $notificationActionClicked = null;
 
     protected $listeners = [
         'native:'.NotificationClicked::class => 'notificationClicked',
+        'native:'.NotificationReply::class => 'notificationReplied',
+        'native:'.NotificationActionClicked::class => 'notificationActionClicked',
     ];
 
     public function sendNotification()
     {
-        Notification::new()
+        $notification = Notification::new()
             ->title($this->title)
-            ->message($this->message)
-            ->show();
+            ->message($this->message);
+
+        if ($this->hasReply) {
+            $notification = $notification->hasReply(
+                $this->replyPlaceholder ?? ''
+            );
+        }
+
+        $actions = array_filter($this->actions);
+        if (count($actions)) {
+            foreach ($actions as $action) {
+                $notification = $notification->addAction($action);
+            }
+        }
+
+        $notification->show();
     }
 
     public function framelessWindow()
@@ -49,6 +77,18 @@ class Notifications extends Component
     public function notificationClicked()
     {
         $this->notificationClicked = true;
+    }
+
+    public function notificationReplied(string $reference, string $reply)
+    {
+        $this->notificationReplied =
+            'Reference: '.$reference.' / Reply: '.$reply;
+    }
+
+    public function notificationActionClicked(string $reference, string $index)
+    {
+        $this->notificationReplied =
+            'Reference: '.$reference.' / Index of the clicked button: '.$index;
     }
 
     public function render()
